994231f02e877056aa3e15b56c269431358301e3
Netesh Paudel <you@example.com>
2021-11-30 13:53:40 +0545

    operators

---
+++ operators.dart
@@ -0,0 +1,50 @@
+void main() {
+  //Arthimatic operators
+  int a = 5;
+  int b = 10;
+  int c = 25;
+  int d = 5;
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(b % a);
+  print(b ~/ a);
+  print("----------------");
+  //prefix and postfix increment and decrement operators
+
+  int i = 5;
+  print(i); //i=5
+  print(i++); // First prints i=5 then increment its value to 6
+  print(++i); //First increment its value then prints i=7
+
+  print(--i); //i==6
+  print(i--); //i=6
+  print(i); //i=5
+  print("----------------");
+
+  //Realtional and Equality operators
+  var num = 100;
+  var num2 = 150;
+  print(num == num2);
+  print(num != num2);
+  print(num < num2);
+  print(num > num2);
+  print(num <= num2);
+  print(num >= num2);
+
+  print("----------------");
+  //Type cast operators
+  var string = "Nitesh";
+  var num3 = 5;
+  print(string is String);
+  print(num is! int);
+
+  print("----------------");
+//Assigment operator
+  var n = 5;
+  n += 5;
+  print(n);
+  n -= 5;
+  print(n);
+}
