eb9a10dbedbfd31789943a852da5e1f57054a204
Netesh Paudel <netesh.paudel@gmail.com>
2021-12-04 16:42:50 +0545

    method_overiding

---
+++ OOP/method_overriding.dart
@@ -0,0 +1,22 @@
+class parent {
+  int num = 5;
+  void display() {
+    print("I am parent");
+  }
+}
+
+class child extends parent {
+  int num = 10;
+  @override
+  void display() {
+    print("I am child");
+    //super keyword to access parent variable with same name in child class
+    print(super.num);
+    print("child num: $num");
+  }
+}
+
+void main() {
+  child c = child();
+  c.display();
+}
